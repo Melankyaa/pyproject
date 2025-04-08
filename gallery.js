4f7b59186fddf323fc2e849687605e1cfe121ecf
Mel <melaniagvinika123@gmail.com>
2025-04-08 19:33:11 +0400

    gallery.js

--- gallery.js
+++ gallery.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
-      items: 3,            // Number of items to show
-      loop: true,          // Loop the items
-      margin: 10,          // Space between items
-      nav: true,           // Show next/prev buttons
-      dots: true,          // Show dots below the carousel
-      autoplay: true,      // Auto-play carousel
-      autoplayTimeout: 3000, // Auto-play interval (in ms)
-      autoplayHoverPause: true // Pause on hover
+      items: 3,            
+      loop: true,         
+      margin: 10,         
+      nav: true,      
+      dots: true,          
+      autoplay: true,      
+      autoplayTimeout: 3000, 
+      autoplayHoverPause: true 
         })
     });
